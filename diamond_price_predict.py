22a4b88a237d994eeb4af47ca3b3432ce0fbb8a4
Sahil Singh <sahilraaj745@gmail.com>
2020-05-04 18:51:12 +0530

    Update diamond_price_predict.py

--- diamond_price_predict.py
+++ diamond_price_predict.py
@@ -12,23 +12,31 @@ import time
 
 
 last_time = time.time()
+
+
 #This section gets the csv file from github repository.
 #If this section_code doesn't work you can try to download the csv file and retrieve it locally on your pc.
 url = 'https://raw.githubusercontent.com/sahilsngh/ML_exersize/master/diamonds.csv'
 df = pd.read_csv(url, error_bad_lines=True, index_col=0)
 print(f"Time takas to download the file:{time.time()-last_time} seconds")
 
+
 #creating a dictionaries to get the numerical values for non-numerical columns. 
+last_time = time.time()
 cut_dict = {"Fair": 5 , "Good": 4 , "Very Good": 3 , "Premium": 2 , "Ideal": 1} 
 color_dict = {"D": 1 , "E": 2 , "F": 3 , "G": 4 , "H": 5 , "I": 6 , "J": 7}
 clarity_dict = {"FL": 1 , "IF": 2 , "VVS1": 3, "VVS2": 4 , "VS1": 5 , "VS2": 6 , "SI1": 7, "SI2": 8, "I1": 9 , "I2": 10 , "I3": 11}
+
 
 #Mapping the dict with data_frame with its respected columns.
 df['cut'] = df['cut'].map(cut_dict)
 df['color'] = df['color'].map(color_dict)
 df['clarity'] = df['clarity'].map(clarity_dict)
+print(f"Time takas to process the file:{time.time()-last_time} seconds")
+
 
 #Shuffling the data_frame.
+last_time = time.time()
 df = sklearn.utils.shuffle(df)
 
 X = df.drop("price", axis=1).values
@@ -43,20 +51,33 @@ y_train = y[:-test_size]
 X_test = X[-test_size:]
 y_test = y[-test_size:]
 
+
 #You can try-         "rbf"   instead of linear.         
 clf = svm.SVR(kernel="linear")
 clf.fit(X_train, y_train)
+print(f"Time takas to train the Model:{time.time()-last_time} seconds")
+
 
 #Print success rate of your model prediction
 print(f"Success rate: {clf.score(X_test, y_test) * 100}")
 
+
 #To check your model prediction for first 500 batch.
 a = 0
 for X,y in zip(X_train, y_train):
-  print(f"Model: {clf.predict([X])[0]},Actaul: {y}")
+  print(f"Model: {clf.predict([X])[0]},Actaul: {y} and the difference in prediction {y-clf.predict([X])[0]}")
   a += 1
   if a == 500:
     break
+
+
+
+
+
+
+
+
+
 
 
 
